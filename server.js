4b3f414aa7f8986caf0373b0be639e38b2cf1259
Ian Hwang <meen813@hotmail.com>
2023-05-04 01:24:40 -0700

    implement login feature

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
-
+//method-override => html 에서 put 과 delete 사용가능케 해줌
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 //ejs 파일 사용
 app.set('view engine', 'ejs');
 
@@ -83,24 +85,24 @@ app.get('/list', (req, res) => {
     })
 });
 
-app.delete('/delete', function(req, res){
+app.delete('/delete', function (req, res) {
     // 요청시 함께 보낸 데이터를 찾으려면...(게시물 번호)
     console.log(req.body)
     //숫자로 변환
-    req.body._id = parseInt(req.body._id); 
+    req.body._id = parseInt(req.body._id);
     // 요청 .body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해 주세요
-    db.collection('post').deleteOne(req.body, function(error, result){
+    db.collection('post').deleteOne(req.body, function (error, result) {
         console.log('Deletion Complete');
         //요청 성공 200 or status(400) = fail
-        res.status(200).send({message : '성공했습니다'});
+        res.status(200).send({ message: '성공했습니다' });
 
-        //counter 라는 collection 에 있는 totalPost 라는 항목을 1 감소 시켜야 함
-        db.collection('counter').updateOne({ name: 'postNum'}, { $inc: {totalPost: -1}}, function(error, result) {
-            if(error){
-                return console.log(error)
-            }
-            console.log('update complete')
-        })
+        // //counter 라는 collection 에 있는 totalPost 라는 항목을 1 감소 시켜야 함
+        // db.collection('counter').updateOne({ name: 'postNum'}, { $inc: {totalPost: -1}}, function(error, result) {
+        //     if(error){
+        //         return console.log(error)
+        //     }
+        //     console.log('update complete')
+        // })
     })
 
 
@@ -109,15 +111,78 @@ app.delete('/delete', function(req, res){
 
 //detail 로 접속하면 detail.ejs 보여줌
 //parameter 사용
-app.get('/detail/:id', function(req, res){
-    db.collection('post').findOne({_id: parseInt(req.params.id)}, function(error, result){
+app.get('/detail/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
         if (error) {
             return console.log(error);
         }
         if (!result) {
-            return res.status(404).send({message:'없는 자료입니다'});
-        }   
+            return res.status(404).send({ message: '없는 자료입니다' });
+        }
         console.log(result);
         res.render('detail.ejs', { data: result });
     })
 })
+
+app.get('/edit/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
+        if (error) {
+            return console.log(error);
+        }
+        if (!result) {
+            return res.status(404).send({ message: '없는 자료입니다' });
+        }
+        console.log(result);
+        res.render('edit.ejs', { data: result });
+    })
+})
+
+app.put('/edit', function (req, res) {
+    //폼에담긴 제목, 날짜 데이터를 가지고 db.collection에 업데이트
+    db.collection('post').updateOne({ _id: parseInt(req.body.id) }, { $set: { title: req.body.title, date: req.body.date } }, function (error, result) {
+        console.log("Edit Complete");
+        res.redirect('/list');
+
+    })
+})
+
+//session 방식 로그인 기능 구현
+//1. 설치한 3개의 라이브러리 첨부하기
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session');
+
+//미들웨어(app.use()); 요청-응답 중간에 뭔가 실행되는 코드
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/login', function (req, res) {
+    res.render('login.ejs');
+})
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/fail'
+}), function (req, res) {
+    res.redirect('/')
+})
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+        if (에러) return done(에러)
+
+        if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+        if (입력한비번 == 결과.pw) {
+            return done(null, 결과)
+        } else {
+            return done(null, false, { message: '비번틀렸어요' })
+        }
+    })
+}));
